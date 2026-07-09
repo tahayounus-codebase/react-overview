cdca879f624ff3810113e1821f8578c191dd6fb7
tahayounus-codebase <tahayou832@gmail.com>
2026-07-09 14:36:08 +0500

    first commit

---
+++ 01-project/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+    <div id='parent'>
+      <h1 id='child1'>Hello</h1>
+      <h2 id='child2'>Hello</h2>
+    </div>
+
+    <div id="chacha"></div>
+    </>
+  );
+};
+
+export default App;
