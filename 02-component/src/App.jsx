73a32b84c825f861307bccd5dd030687cb1a83b6
tahayounus-codebase <tahayou832@gmail.com>
2026-07-15 11:47:28 +0500

    learned about component

---
+++ 02-component/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Card from './components/Card'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Navbar/>
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ 02-component/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div className="card">
+        <h1>HELLO,Taha Younus  </h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat earum consequuntur debitis iure similique quis?</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
